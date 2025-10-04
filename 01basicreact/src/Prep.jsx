99fb6a3d4e8d9581f1dc81379c664c96bbf18edf
Anju <anju22bai71319@gmail.com>
2025-10-04 16:22:43 +0530

    moving to a new folder

--- 01basicreact/src/Prep.jsx
+++
@@ -1,4 +0,0 @@
-function Chai() {
-    return (<h3>preparing in react</h3>)
-}
-export default Chai
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function Myapp() {
+  return (
+    <div>
+      <h1>Custom app</h1>
+      </div>
+  )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Myapp/>
 )
+
+ 
