817996264b39606a79d69146d2f6c3b3d6be1333
Anju <anju22bai71319@gmail.com>
2025-10-06 20:46:19 +0530

    commit all changes in the files

---
+++ 01basicreactnew/src/module1.mjs
@@ -0,0 +1,3 @@
+import dx,{a as a1,b as b1,d as d1} from './module2.mjs' 
+console.log(dx);
+console.log(a1, b1, d1);
---
+++ 01basicreactnew/src/module2.mjs
@@ -0,0 +1,9 @@
+const a="harry"
+const b="rohan"
+const c = "shubham"
+const d = "mohan"
+export default c;
+export { a };
+export { b };
+export { d };
+    
